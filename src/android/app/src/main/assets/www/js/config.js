1c31e12afa193e54ccc3e478123bad2272003c7d
arivazhagan <arivu@example.com>
2018-03-24 17:33:19 +0530

    First Commit

--- src/android/app/src/main/assets/www/js/config.js
+++
@@ -1,6 +0,0 @@
-var krms_config ={				
-    'ApiUrl': "https://togoorders.online/merchantapp/api",
-    'DialogDefaultTitle': "FoodFox",
-	'pushNotificationSenderid': "AIzaSyDpyppHmMpQFGYOvcL-DWgmogalAaVeN18",
-	'APIHasKey': "e0136e91c0e09554f6b6224599d45d0e"
-};
